a29f235e23ebfcb4ef17cc4942b9dbb30fb9fab0
T Lakshmi P <108550283+Lakshmip22021@users.noreply.github.com>
2022-10-13 22:08:55 +0100

    Update streamlit_app.py
    adding a csv file using pandas

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,10 @@
 
 import streamlit
+import pandas
+
 streamlit.title("🥣  My Parents New Healthy Diner")
 #streamlit.title(" just to need to check")
-
 streamlit.header(' My Menu in Diner')
-
 streamlit.text('Healthy food options :🥣  Kale, Spinach Smoothie')
 streamlit.text('🥗 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥣 Kale, Spinach & Rocket Smoothie')
@@ -12,4 +12,6 @@ streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
 
