691983609c0df01153e31485f6e91883415c492d
T Lakshmi P <108550283+Lakshmip22021@users.noreply.github.com>
2022-10-13 12:44:44 +0100

    Update streamlit_app.py
    added header text

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,5 +3,5 @@ import streamlit
 streamlit.title(" My Parents New Healthy Diner")
 #streamlit.title(" just to need to check")
 
-streamlit.title(' My Menu in Diner')
+streamlit.header(' My Menu in Diner')
 
