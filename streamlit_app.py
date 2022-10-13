0dacf4d63a7aa36abebb7ef8fbaff94aa212a8ba
T Lakshmi P <108550283+Lakshmip22021@users.noreply.github.com>
2022-10-13 21:48:24 +0100

    Update streamlit_app.py
    added text msg

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,4 @@ streamlit.title(" My Parents New Healthy Diner")
 #streamlit.title(" just to need to check")
 
 streamlit.header(' My Menu in Diner')
-
+streamlit.text('Healthy food options : Kale, Spinach Smoothie')
