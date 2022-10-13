8709ec13e259d047e1ec18eec774878a650cef3e
T Lakshmi P <108550283+Lakshmip22021@users.noreply.github.com>
2022-10-13 12:43:25 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 
 import streamlit
 streamlit.title(" My Parents New Healthy Diner")
-streamlit.title(" just to need to check")
+#streamlit.title(" just to need to check")
 
 streamlit.title(' My Menu in Diner')
 
