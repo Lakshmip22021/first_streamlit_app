732255c57c5aec0bca2a87d9bdc9855345d22488
T Lakshmi P <108550283+Lakshmip22021@users.noreply.github.com>
2022-10-19 11:05:23 +0100

    Update streamlit_app.py
    removed default kiwi

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,7 +49,7 @@ streamlit.text("The Fruit Load List Contains:")
 #streamlit.text(my_data_row)
 streamlit.dataframe(my_data_row)
 
-fruit_choice = streamlit.text_input('What fruit would you like to add?','Kiwi')
+fruit_choice = streamlit.text_input('What fruit would you like to add?')
 streamlit.write('Thanks for adding ', fruit_choice)
 
 
