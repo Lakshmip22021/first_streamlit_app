05a39173e7108087cacb76b8f334b856294de1b5
T Lakshmi P <108550283+Lakshmip22021@users.noreply.github.com>
2022-10-19 11:32:21 +0100

    Update streamlit_app.py
    try and changing the view of page

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
+from urllib.error import URLError
 
 streamlit.title("🥣  My Parents New Healthy Diner")
 #streamlit.title(" just to need to check")
@@ -37,6 +38,8 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_cho
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # dataframe is send to streamlit
 streamlit.dataframe(fruityvice_normalized)
+
+streamlit.stop()
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
